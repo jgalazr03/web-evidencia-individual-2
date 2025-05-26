b23d84bb96415306ea0fee6b3a35741cf315ecfa
Jesus Galaz <jesusgalazr@icloud.com>
2025-05-26 16:37:09 -0600

    Mejorando el server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,15 +2,20 @@ import express from "express";
 import cors from "cors";
 import { PrismaClient } from "@prisma/client";
 import pollsRouter from "./routes/polls";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const prisma = new PrismaClient();
+
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/polls", pollsRouter);
 
-app.listen(4000, () => {
-  console.log("🚀 Backend corriendo en http://localhost:4000");
+app.listen(PORT, () => {
+  console.log(`🚀 Backend corriendo en http://localhost:${PORT}`);
 });
